affea858e6aa0c18e86d172b78704b9552099d2d
Andrew Konoff <andrew.konoff@gmail.com>
2017-08-15 17:33:26 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	private int count;
+	public Text countText;
+	public Text winText;
+	public float speed;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+		// Check every frame for player movement.
+	}
+
+	void FixedUpdate () {
+		// Here's where the physics update goes.
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("PickUp")) {
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText();
+		}
+	}
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 12) {
+			winText.text = "You Win !!!!";
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+	// let's keep the rotation
+	// but change the transform
+	transform.Rotate(new Vector3(15,30,45) * Time.deltaTime);
+	}
+}
